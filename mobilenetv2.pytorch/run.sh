e2a3006f5867447dc4d42cfd6d190980afc64ddd
5663015 <398155058@qq.com>
2020-05-30 23:06:35 +0800

    fix mobilenetv2.pytorch

--- mobilenetv2.pytorch/run.sh
+++ mobilenetv2.pytorch/run.sh
@@ -1,13 +1,13 @@
 source activate torch1.0
 export CUDA_VISIBLE_DEVICES=1,2 && nohup python -u imagenet.py -a mobilenetv2 \
     -b 256 \
-    -d /dataset/imagenet \
+    -d /home/work/dataset/ILSVRC2012 \
     --epochs 150 \
     --lr-decay cos \
     --lr 0.05 \
     --wd 4e-5 \
-    -c ./snapshots \
+    -c ../models \
     --width-mult 1.0 \
     --input-size 224 \
-    -j 32 > logdir/a0.log 2>&1 &
-tail -f logdir/a0.log
+    -j 32 > a0.log 2>&1 &
+tail -f a0.log
